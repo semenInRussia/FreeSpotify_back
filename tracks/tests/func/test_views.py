0ec13286f421a13c18317d3a30b5d35a9ad1553f
semenInRussia <semenprofis999@gmail.com>
2020-10-17 16:48:34 +0300

    add test search_track_view()

--- tracks/tests/func/test_views.py
+++ tracks/tests/func/test_views.py
@@ -10,8 +10,21 @@ artist_name = 'ac dc'
 album_name = 'back in black'
 track_name = 'hells bells'
 
-track_view_url = f"http://localhost:8000/api/tracks/{artist_name}/{album_name}/{track_name}"
+base_url = 'http://localhost:8000/api/tracks'
 
+track_view_url = f"{base_url}/{artist_name}/{album_name}/{track_name}"
+search_view_url = f"{base_url}/search/{artist_name} - {track_name}"
+
+def test_search_track_view():
+    response = requests.get(search_view_url)
+
+    assert_is_valid_request(response)
+
+    data = json.loads(response.text)
+
+    assert "name" in data[0]
+    assert "album_name" in data[0]
+    assert "artist_name" in data[0]
 
 def test_get_track_view():
     response = requests.get(track_view_url)
