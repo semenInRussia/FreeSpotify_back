757aca612f481d2ecdd5f3ed27963d5d0dc49dd1
semenInRussia <semenprofis999@gmail.com>
2020-10-19 20:00:53 +0300

    add test_get_precise_album_name()

--- buisness_logic/tests/func/test_artits.py
+++ buisness_logic/tests/func/test_artits.py
@@ -1,12 +1,19 @@
 from buisness_logic.Artist import Artist
 
 artist_name = "Metallica"
+approximate_artist_name = "metalica"
 
 
 def test_init():
+    # Not raise exception
     Artist(artist_name)
 
 def test_get_album_name():
     artist = Artist(artist_name)
+
+    assert artist.name == artist_name
+
+def test_get_precise_album_name():
+    artist = Artist(approximate_artist_name)
 
     assert artist.name == artist_name
