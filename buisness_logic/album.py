692e768c3768e891623e0e346df82ee0ca1c3837
semenInRussia <semenprofis999@gmail.com>
2020-10-20 22:27:59 +0300

    implement name

--- buisness_logic/album.py
+++ buisness_logic/album.py
@@ -2,3 +2,7 @@ class Album:
     def __init__(self, artist_name: str, album_name: str):
         self._artist_name = artist_name
         self._album_name = album_name
+
+    @property
+    def name(self):
+        return self._album_name
