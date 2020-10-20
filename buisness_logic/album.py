f03881d1f3fc7d5e4b1206469af036919caff47f
semenInRussia <semenprofis999@gmail.com>
2020-10-20 23:42:13 +0300

    rename _spotify_id to spotifu_id in Album

--- buisness_logic/album.py
+++ buisness_logic/album.py
@@ -2,7 +2,7 @@ class BaseAlbum:
     def __init__(self, artist_name: str, album_name: str, spotify_id: str = None):
         self._artist_name = artist_name
         self._album_name = album_name
-        self._spotify_id = spotify_id
+        self.spotify_id = spotify_id
 
 class Album(BaseAlbum):
     @property
