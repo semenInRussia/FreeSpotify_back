32a64576828a82329a4a2786e39bf46951a18d78
semenInRussia <semenprofis999@gmail.com>
2020-12-15 12:39:12 +0300

    change music_manager_impl on RocknationAndSpotify

--- settings/general.py
+++ settings/general.py
@@ -1,9 +1,8 @@
 from music_manger.implementations.RocknationAndSpotify.rocknation_and_spotify import RocknationAndSpotify
-from music_manger.implementations.mock import MockMusicManager
 from settings.flask import default_port
 
 description = "This is app for to download music from spotify."
 epilog = 'Good luck!'
 help_text_for_port = f"This is port, he will open. (default: {default_port})"
 
-music_manager_impl = MockMusicManager
+music_manager_impl = RocknationAndSpotify
