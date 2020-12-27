00cdcf2e9701791204d99558994a6e0b470ff149
semenInRussia <semenprofis999@gmail.com>
2020-12-27 20:41:43 +0300

    add stickers to bot, refactor /bot/main.py

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,12 @@
 import logging
+import re
 from typing import List
 
 from aiogram import Bot, Dispatcher, executor, types
 
 # Configure logging
 from entities import Artist, Track
-from settings.bot import BOT_TOKEN, BOT_DESCRIPTION
+from settings.bot import BOT_TOKEN, BOT_DESCRIPTION, stickers
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,23 +17,45 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply(BOT_DESCRIPTION)
+    await message.answer_sticker(stickers.WELCOME)
+    await message.answer(BOT_DESCRIPTION)
 
 
-def _get_string_track(track: Track, index: int) -> str:
-    return (
-        f"{index + 1}. {track.name} \n"
-        f"     ({track.album.name} [{track.album.release_date}])\n"
-        f"     {track.album.link}\n"
-    )
+@dp.message_handler(commands=['top'])
+async def get_top(message: types.Message):
+    try:
+        message_of_answer = _get_detail_artist_message_by_message(message)
+    except AttributeError:
+        await message.answer("You are inputted not valid band's name.")
+    except Exception as e:
+        await message.answer_sticker(stickers.FAIL)
+        await message.answer("Sorry, I am can't search info about. \n"
+                             f"Error: {e.__class__.__name__}. \n")
+    else:
+        await message.answer(message_of_answer)
 
 
-def _get_string_top(top: List[Track]) -> str:
-    return "\n".join(
-        [
-            _get_string_track(track, i) for i, track in enumerate(top)
-        ]
-    )
+def _get_detail_artist_message_by_message(message: types.Message) -> str:
+    artist_name = _find_artist_name_in_message(message.text)
+    message_of_answer = _get_detail_artist_message_by_name(artist_name)
+
+    return message_of_answer
+
+
+def _find_artist_name_in_message(message: str):
+    result = re.search(r'/top +(.+)', message)
+    return result.group(1)
+
+
+def _get_detail_artist_message_by_name(artist_name) -> str:
+    artist = Artist(artist_name)
+    message_of_answer = _get_detail_artist_message(artist)
+
+    return message_of_answer
+
+
+def _get_detail_artist_message(artist: Artist):
+    return _get_message_about_artist(artist) + _get_string_top(artist.top)
 
 
 def _get_message_about_artist(artist: Artist):
@@ -43,17 +66,21 @@ def _get_message_about_artist(artist: Artist):
     )
 
 
-def _get_detail_artist_message(artist: Artist):
-    return _get_message_about_artist(artist) + _get_string_top(artist.top)
+def _get_string_top(top: List[Track]) -> str:
+    string_top = ""
+
+    for i, track in enumerate(top):
+        string_top += _get_string_track(track, i)
+
+    return string_top
 
 
-@dp.message_handler()
-async def get_top(message: types.Message):
-    artist = Artist(message.text)
-
-    message_of_answer = _get_detail_artist_message(artist)
-
-    await message.answer(message_of_answer)
+def _get_string_track(track: Track, index: int) -> str:
+    return (
+        f"{index + 1}. {track.name} \n"
+        f"     ({track.album.name} [{track.album.release_date}])\n"
+        f"     {track.album.link}\n"
+    )
 
 
 def run():
--- settings/bot.py
+++ settings/bot.py
@@ -9,3 +9,8 @@ BOT_NAME = "Music manager [FREE]"
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 BOT_DESCRIPTION = """I am very COOL bot. My name is {name} and I can taking Rock n' Roll music.""".format(name=BOT_NAME)
+
+
+class stickers:
+    FAIL = "CAACAgIAAxkBAAEBuVlf6MEBtd8e94ObW5LSmP1_FrWZHAACgQMAAs-71A6WvjEQeKbJyh4E"
+    WELCOME = "CAACAgIAAxkBAAEBuVtf6MZ8_6rAiFA6uBh9uxtluKhr7wACSgEAApafjA6Mfk73uDljvh4E"
