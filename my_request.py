5e8175ffd29ba86338b26fcfc97e1f9b43ca719f
Semen <hrams205@gmail.com>
2021-04-18 18:31:49 +0300

    [MY REQUESTS: refactoring] Create var dependencies_of_methods_on_name, for agile of module

--- my_request.py
+++ my_request.py
@@ -1,17 +1,25 @@
 import json
+from typing import Dict, Callable
 
 import requests
 from bs4 import BeautifulSoup
 
+dependencies_of_methods_on_name: Dict[str, Callable] = {
+    "get": requests.get,
+    "post": requests.post,
+    "put": requests.put,
+    "delete": requests.delete,
+}
 
-def get_bs(url: str, method_name: str = "get", **kwargs) -> BeautifulSoup:
+
+def get_bs(url: str, method_name: str = 'get', **kwargs) -> BeautifulSoup:
     html = get_content(url, method_name, **kwargs)
 
     return BeautifulSoup(html, "html.parser")
 
 
-def get_json(url: str, method_name: str = 'get'):
-    return json.loads(get_content(url, method_name))
+def get_json(url: str, method_name: str = 'get', **kwargs):
+    return json.loads(get_content(url, method_name, **kwargs))
 
 
 def get_content(url: str, method_name: str = 'get', **kwargs):
@@ -19,4 +27,10 @@ def get_content(url: str, method_name: str = 'get', **kwargs):
 
 
 def create_request(url: str, method_name: str = 'get', **kwargs):
-    return requests.request(method_name, url, **kwargs)
+    method = get_method_by_name(method_name)
+
+    return method(url, **kwargs)
+
+
+def get_method_by_name(method_name: str = 'get') -> Callable:
+    return dependencies_of_methods_on_name.get(method_name)
