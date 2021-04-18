2ce884dc01af4826f08bfc1c03da5828854b689e
Semen <hrams205@gmail.com>
2021-04-18 21:50:31 +0300

    [SPOTIFY CORE: refactoring] Add docs to spotify API methods

--- music_manger/implementations/rocknation_and_spotify/spotify/spotifyCore.py
+++ music_manger/implementations/rocknation_and_spotify/spotify/spotifyCore.py
@@ -35,6 +35,7 @@ def _check_json_spotify_response(json_response: dict):
 
     raise UndefinedErrorMessageException
 
+
 def _is_response_has_error(json_response: dict):
     return json_response.get('error')
 
@@ -118,7 +119,7 @@ class SpotifyJsonParser:
 
 class SpotifyCore:
     """
-    Object for work for base operation with spotify.
+    Object for work with base operations with spotify on level with rest api.
 
     For additional info watch https://api.spotify.com/v1/search.
     """
@@ -127,6 +128,53 @@ class SpotifyCore:
         self._json_parser = SpotifyJsonParser()
 
     def search(self, q: str, type_: str, market: str = None, limit: int = 1, offset: int = 0) -> dict:
+        """
+        Search ANY in Spotify.
+
+        Info from https://developer.spotify.com/documentation/web-api/reference/#endpoint-search .
+
+        :param q: str
+        Search query keywords and optional field filters and operators.
+        For example:
+        q="AC DC - Down Payment Blue".
+        :param type_: str
+        A comma-separated list of item types to search across.
+
+        Valid types are:
+        * album
+        * artist
+        * playlist
+        * track
+        * show
+        * episode.
+
+        Search results include hits from all the specified item types.
+
+        :param market: str | None
+        An ISO 3166-1 alpha-2 country code or the string from_token.
+        If a country code is specified, only content that is playable in that market is returned.
+        Note:
+        - Playlist results are not affected by the market parameter.
+        - If market is set to from_token, and a valid access token is specified in the request header, only content
+          playable in the country associated with the user account, is returned.
+        - Users can view the country that is associated with their account in the account settings. A user must grant
+          access to the user-read-private scope prior to when the access token is issued. 	String 	Optional
+
+        :param limit: int | None
+        Maximum number of results to return.
+        * Default: 20
+        * Minimum: 1
+        * Maximum: 50
+        Note: The limit is applied within each type, not on the total response.
+        For example, if the limit value is 3 and the type is artist,album, the response contains 3 artists and 3 albums. 	Integer 	Optional
+
+        :param offset: int
+        The index of the first result to return.
+        * Default: 0 (the first result).
+        * Maximum offset (including limit): 1,000.
+        Use with limit to get the next page of search results.
+       """
+
         return self._json_parser.get_json_from_spotify(
             second_part_of_links='search',
 
@@ -137,16 +185,39 @@ class SpotifyCore:
             market=market
         )
 
-    def get_top_tracks(self, artist_id: str, country: str = 'US') -> dict:
+    def get_top_tracks(self, artist_id: str, market: str = 'US') -> dict:
+        """
+        Get Artist's top.
+
+        Info from https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-an-artists-top-tracks .
+
+        :param artist_id: str
+        The Spotify ID for the artist 	String 	Required
+        Query Parameter 	Type 	Required
+        :param market: str | None
+        An ISO 3166-1 alpha-2 country code or the string from_token. Synonym for country.
+        """
+
         url = f'artists/{artist_id}/top-tracks'
 
         return self._json_parser.get_json_from_spotify(
             second_part_of_links=url,
 
-            country=country
+            country=market
         )
 
     def get_album_info(self, album_ids: str, market: str = 'ES'):
+        """
+        Get info about current albums by ids.
+        Info from https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-multiple-albums
+
+        :param album_ids: str
+        A comma-separated list of the Spotify IDs for the albums. Maximum: 20 IDs.
+        :param market: str | None
+        An ISO 3166-1 alpha-2 country code or the string from_token. Provide this parameter if you want to apply Track
+        Relinking.
+        """
+
         return self._json_parser.get_json_from_spotify(
             second_part_of_links='albums',
 
