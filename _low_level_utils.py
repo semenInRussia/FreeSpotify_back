78d221a2f431f247b7c9904551f5758369375e47
Semen <hrams205@gmail.com>
2021-04-25 23:20:10 +0300

    [LOW LEVER: feature] Added get_public_fields_of()

--- _low_level_utils.py
+++ _low_level_utils.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def sum_of_lists(*lists):
     result = []
 
@@ -5,3 +8,14 @@ def sum_of_lists(*lists):
         result.extend(current_list)
 
     return result
+
+
+def get_public_fields_of(obj, ignore: List[str] = []):
+    all_fields = dir(obj)
+    is_public_field_name = lambda field_name: not (field_name.startswith("_") or field_name in ignore)
+
+    return list(filter(
+        is_public_field_name,
+
+        all_fields
+    ))
--- tests/test_low_level_utils.py
+++ tests/test_low_level_utils.py
@@ -1,5 +1,18 @@
+from _low_level_utils import get_public_fields_of
 from _low_level_utils import sum_of_lists
 
+class TestClass:
+    def __init__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    _private_field1 = None
+    _private_field2 = None
+
+    public_field = None
+    protected_field = None
 
 def test_sum_of_lists():
     assert sum_of_lists([1, 2]) == [1, 2]
@@ -8,3 +21,7 @@ def test_sum_of_lists():
     assert sum_of_lists(["str1", "str2"]) == ["str1", "str2"]
 
     assert sum_of_lists(*[['dir\\subdir1', 'dir\\subdir2']]) == ['dir\\subdir1', 'dir\\subdir2']
+
+
+def test_get_public_fields_of():
+    assert get_public_fields_of(TestClass, ignore=["protected_field"]) == ["public_field"]
