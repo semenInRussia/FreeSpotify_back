2eba326189fcbbf499d9da7ebae2f4cc8e085b03
Semen <hrams205@gmail.com>
2021-04-26 19:26:26 +0300

    [LOW LEVEL: feature] Created Cash class

--- _low_level_utils.py
+++ _low_level_utils.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 def sum_of_lists(*lists):
     result = []
 
@@ -10,7 +7,10 @@ def sum_of_lists(*lists):
     return result
 
 
-def get_public_fields_of(obj, ignore: List[str] = []):
+def get_public_fields_of(obj, ignore=None):
+    if ignore is None:
+        ignore = []
+
     all_fields = dir(obj)
     is_public_field_name = lambda field_name: not (field_name.startswith("_") or field_name in ignore)
 
@@ -19,3 +19,25 @@ def get_public_fields_of(obj, ignore: List[str] = []):
 
         all_fields
     ))
+
+
+class Cash:
+    def __init__(self, default_values=None):
+        if default_values is None:
+            default_values = []
+
+        self._values = default_values
+
+    def set(self, key, value):
+        self._values[key] = value
+
+    def get(self, key, func_return_result_if_undefined=None):
+        if func_return_result_if_undefined is None:
+            func_return_result_if_undefined = lambda current_key: None
+
+        found_key = self._values.get(key)
+
+        if found_key:
+            return found_key
+        else:
+            return func_return_result_if_undefined(key)
