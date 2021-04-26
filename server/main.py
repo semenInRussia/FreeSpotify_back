1e0a38d1d3124e4ccb42dbef81224ba8992bc5cc
Semen <hrams205@gmail.com>
2021-04-26 17:03:08 +0300

    [SERVER/serializers: refactoring] Renamed variables in server.urls

--- server/main.py
+++ server/main.py
@@ -1,10 +1,20 @@
-from flask_cors import CORS, cross_origin
 from flask import Flask
+from flask_cors import CORS
+from flask_cors import cross_origin
 
-from entities import Track, Artist, Album
+from entities import Album
+from entities import Artist
+from entities import Track
+
 from server.serializers.entities_serializers import EntitiesSerializer
-from server.urls import MAIN_PAGE_URL, ARTISTS_URL, ARTIST_DETAIL_URL, ALBUMS_URL, ALBUM_DETAIL_URL
-from server.urls import TRACKS_URL, TRACK_DETAIL_URL
+
+from server.urls import ALBUMS_WELCOME_PAGE_URL
+from server.urls import ALBUM_DETAIL_PAGE_URL
+from server.urls import ARTISTS_WELCOME_PAGE_URL
+from server.urls import ARTIST_DETAIL_PAGE_URL
+from server.urls import MAIN_PAGE_URL
+from server.urls import TRACKS_WELCOME_PAGE_URL
+from server.urls import TRACK_DETAIL_PAGE_URL
 
 app = Flask(__name__)
 
@@ -17,21 +27,21 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def main_handle():
     return {
         'Main page': MAIN_PAGE_URL,
-        'Artists': ARTISTS_URL,
-        'Albums': ALBUMS_URL,
-        'Tracks': TRACKS_URL
+        'Artists': ARTISTS_WELCOME_PAGE_URL,
+        'Albums': ALBUMS_WELCOME_PAGE_URL,
+        'Tracks': TRACKS_WELCOME_PAGE_URL
     }
 
 
-@app.route(ARTISTS_URL)
+@app.route(ARTISTS_WELCOME_PAGE_URL)
 @cross_origin()
 def artists_handle():
     return {
-        'Watch detail info': ARTIST_DETAIL_URL
+        'Watch detail info': ARTIST_DETAIL_PAGE_URL
     }
 
 
-@app.route(ARTIST_DETAIL_URL)
+@app.route(ARTIST_DETAIL_PAGE_URL)
 @cross_origin()
 def artist_detail_handle(artist_name):
     artist = Artist(artist_name)
@@ -40,15 +50,15 @@ def artist_detail_handle(artist_name):
     return serializer.get_data()
 
 
-@app.route(ALBUMS_URL)
+@app.route(ALBUMS_WELCOME_PAGE_URL)
 @cross_origin()
 def albums_handle():
     return {
-        'Detail': ALBUM_DETAIL_URL
+        'Detail': ALBUM_DETAIL_PAGE_URL
     }
 
 
-@app.route(ALBUM_DETAIL_URL)
+@app.route(ALBUM_DETAIL_PAGE_URL)
 @cross_origin()
 def album_detail_handle(artist_name: str, name: str):
     album = Album(artist_name, name)
@@ -57,15 +67,15 @@ def album_detail_handle(artist_name: str, name: str):
     return serializer.get_data()
 
 
-@app.route(TRACKS_URL)
+@app.route(TRACKS_WELCOME_PAGE_URL)
 @cross_origin()
 def tracks_handle():
     return {
-        'Watch All Info': TRACK_DETAIL_URL
+        'Watch All Info': TRACK_DETAIL_PAGE_URL
     }
 
 
-@app.route(TRACK_DETAIL_URL)
+@app.route(TRACK_DETAIL_PAGE_URL)
 @cross_origin()
 def track_detail_handle(artist_name, album_name, name):
     track = Track(artist_name, album_name, name)
--- server/urls.py
+++ server/urls.py
@@ -2,14 +2,17 @@ MAIN_PAGE_URL = '/api/'
 
 template_for_url = MAIN_PAGE_URL + "{url}/"
 
-ARTISTS_URL = template_for_url.format(url='artists')
 
-ARTIST_DETAIL_URL = template_for_url.format(url='artists/detail/<artist_name>')
+ARTISTS_WELCOME_PAGE_URL = template_for_url.format(url='artists')
 
-ALBUMS_URL = template_for_url.format(url='albums')
+ARTIST_DETAIL_PAGE_URL = template_for_url.format(url='artists/detail/<artist_name>')
 
-ALBUM_DETAIL_URL = template_for_url.format(url='albums/detail/<artist_name>/<name>')
+ALBUMS_WELCOME_PAGE_URL = template_for_url.format(url='albums')
 
-TRACKS_URL = template_for_url.format(url='tracks')
+ALBUM_DETAIL_PAGE_URL = template_for_url.format(url='albums/detail/<artist_name>/<name>')
 
-TRACK_DETAIL_URL = template_for_url.format(url='tracks/detail/<artist_name>/<album_name>/<name>')
+TRACKS_WELCOME_PAGE_URL = template_for_url.format(url='tracks')
+
+TRACK_DETAIL_PAGE_URL = template_for_url.format(
+    url='tracks/detail/<artist_name>/<album_name>/<name>'
+)
