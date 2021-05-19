904f0a6eccf879026a68a6e2a04843303120bd93
Semen <hrams205@gmail.com>
2021-05-19 22:18:17 +0300

    [LOW LEVEL: feature] Created func - format_exception

--- _low_level_utils.py
+++ _low_level_utils.py
@@ -3,6 +3,20 @@ from functools import lru_cache
 cashed_function = lru_cache()
 
 
+def format_exception(error_name: str, error_description: str) -> str:
+    if error_description:
+        error_description = f"Description - \"{error_description}\""
+    else:
+        error_description = "Description not found..."
+
+    return (
+        f"Sorry... I'm found error...\n"
+        "Detail:\n"
+        f"|     Name - {error_name}\n"
+        f"|     {error_description}"
+    )
+
+
 def sum_of_lists(*lists):
     result = []
 
