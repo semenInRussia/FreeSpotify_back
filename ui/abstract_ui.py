7a550bd9c27998b6c198190b21746dbcd98fff8a
semenInRussia <hrams205@gmail.com>
2022-12-25 19:00:58 +0300

    [ui.abstract] make text ui more complex with commands
    instead of answering name of an artist and print info about him, parse
    user's message, check it's command and do respective thing.
    
    now available only command "/artist", but i already have written agile
    system of commands, so i later easy add a lot of additional command

--- ui/abstract_ui.py
+++ ui/abstract_ui.py
@@ -1,33 +1,32 @@
 import traceback
 from typing import Callable
 
-from entities import Artist
-from entities_formatter import AbstractParseMode
-from entities_formatter import format_artist
-from entities_formatter import get_parse_mode_by_name
-from ui.handler_collection import HandlersCollection
+from ..entities import Artist
+
+from .entities_formatter import AbstractParseMode
+from .entities_formatter import format_artist
+from .entities_formatter import get_parse_mode_by_name
+
+from .handler_collection import HandlersCollection
 
 
 class AbstractUI:
     handlers: HandlersCollection
 
     _parse_mode_name: str = "text"
-    _parse_mode: AbstractParseMode = None
+    _parse_mode: AbstractParseMode
 
     def __init__(self, additional_entities_settings=None):
         self._additional_entities_settings = additional_entities_settings
 
     def run(self):
         self._raise_event_or_call_self_method("start")
-
         self.handlers.execute_calls_queue()
 
         try:
             while True:
                 user_message = self.get_user_message()
-
-                self.print_artist(user_message)
-                self.handlers.execute_calls_queue()
+                self.handle_user_message(user_message)
 
         except KeyboardInterrupt:
             self._raise_event_or_call_self_method("finish")
@@ -36,36 +35,23 @@ class AbstractUI:
 
     def _raise_event_or_call_self_method(self, event_name: str):
         """
-        If has handler on event -> raise event,
-        Else -> run method of current class with '_' + event_name
+        If has handler on event, then raise event,
+        Otherwise run method of current class called event_name
         """
-
         if self.handlers.is_has_handlers_on_events(event_name):
             self.handlers.raise_event(event_name)
-
         else:
-            method_name = '_' + event_name
-            method = getattr(self, method_name)
-
+            method = getattr(self, event_name)
             method()
 
     def get_user_message(self) -> str:
-        """Implement if you control getting user message."""
+        return NotImplemented
 
-    def print_artist(self, artist_name: str):
-        try:
-            message = self._format_artist_by_name(artist_name)
-
-        except Exception as e:
-            self._print_error(e)
-
-        else:
-            self._print_normal_message(message)
-
-    def _format_artist_by_name(self, artist_name: str) -> str:
-        artist = Artist(artist_name)
-
-        return format_artist(artist, self.parse_mode)
+    def handle_user_message(self, msg: str):
+        for command in _commands:
+            if command.is_run(msg):
+                command.execute(msg, self)
+                break
 
     @property
     def parse_mode(self):
@@ -74,22 +60,24 @@ class AbstractUI:
 
         return self._parse_mode
 
-    def _print_normal_message(self, message: str):
+    def print_normal_message(self, message: str):
         self.handlers.raise_event("print normal message", message)
 
-    def _print_error(self, exception: Exception):
+    def print_error(self, exception: Exception):
         traceback.print_exc()
 
         self.handlers.raise_event("print error", exception)
 
-    def _start(self):
-        self._print_normal_message("Hello, Good Luck!")
+    def start(self):
+        self.print_normal_message("Hello, Good Luck!")
 
-    def _finish(self):
-        self._print_normal_message("I am leave this, GOOD BY!")
+    def finish(self):
+        self.print_normal_message("I am leave this, GOOD BY!")
 
 
-def create_ui(handlers: HandlersCollection, get_user_message_func: Callable, parse_mode_name: str = 'text'):
+def create_ui(handlers: HandlersCollection,
+              get_user_message_func: Callable[[], str],
+              parse_mode_name: str = 'text'):
     class UI(AbstractUI):
         def __init__(self, *args, **kwargs):
             self.handlers = handlers
@@ -97,6 +85,46 @@ def create_ui(handlers: HandlersCollection, get_user_message_func: Callable, par
 
             super().__init__(*args, **kwargs)
 
-    UI.get_user_message = staticmethod(get_user_message_func)
+        @staticmethod
+        def get_user_message() -> str:
+            return get_user_message_func()
 
     return UI
+
+
+class _Command:
+    command_name: str
+
+    def is_run(self, msg: str) -> bool:
+        """
+        If in given msg user need to run this command, return True,
+        otherwise False
+        """
+        return msg.startswith(self._command_prefix)
+
+    def execute(self, _msg: str, _ui: AbstractUI):
+        """
+        Execute this command, using preferences written in given msg
+        and methods of object AbstractUI
+        """
+        return NotImplemented
+
+    @property
+    def _command_prefix(self):
+        return "/" + self.command_name
+
+class _SearchArtistCommand(_Command):
+    command_name = "artist"
+    _ui: AbstractUI
+
+    def execute(self, msg: str, ui: AbstractUI):
+        artist_name = msg.removeprefix(self._command_prefix).strip()
+        message = self._format_artist_by_name(artist_name, ui)
+        ui.print_normal_message(message)
+
+    def _format_artist_by_name(self, artist_name: str, ui: AbstractUI) -> str:
+        artist = Artist(artist_name)
+        return format_artist(artist, ui.parse_mode)
+
+
+_commands: list[_Command] = [_SearchArtistCommand()]
